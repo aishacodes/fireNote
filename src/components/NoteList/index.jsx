88c8cf77d0ed4d90a914e564f3340cf49983ddd7
aishacodes <aishatabdulfatah81@gmail.com>
2020-12-05 23:49:47 +0100

    feat: layout

---
+++ src/components/NoteList/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import './NoteList.css'
+
+const Menu = () => {
+
+  return(
+    <div >
+
+    </div>
+  )
+}
+
+
+function index() {
+  return (
+    <div className="NoteList">
+      <Menu />
+    </div>
+  )
+}
+
+export default index
