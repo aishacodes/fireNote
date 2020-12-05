cd84c6f16160fff94a15bf568b979cbb4d7d8ca9
aishacodes <aishatabdulfatah81@gmail.com>
2020-12-05 21:56:32 +0100

    project setup

---
+++ src/components/icons/MenuIcon.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MenuIcon() {
+  return (
+    <svg
+      class="icon-menu"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <rect x="0" fill="none" width="24" height="24"></rect>
+      <g>
+        <path d="M21,11H3v2H21Zm0-5H3V8H21Zm0,10H3v2H21Z"></path>
+      </g>
+    </svg>
+  );
+}
+
+export default MenuIcon;
